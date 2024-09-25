00e0bcbf52fdd32bc1de68787879539858146e61
Mehdi-Chahir <babassako@hotmail.com>
2024-09-25 17:41:32 -0400

    Update TP2.py

--- TP2.py
+++ TP2.py
@@ -43,29 +43,17 @@ csvfile.close()
 # PARTIE 2 : Ajout d'une nouvelle collection à la bibliothèque
 ########################################################################################################## 
 
-# TODO : Écrire votre code ici
-import csv
-
-bibliotheque2={}
 csvfile = open('nouvelle_collection.csv', newline='')
-c2 = csv.DictReader(csvfile)
-
-for row2 in c2:
-    
-    cote_rangement=row2['cote_rangement']
-    bibliotheque2[cote_rangement]={
-        
-    }
+x = csv.DictReader(csvfile)
+for row in x:
+    cote_rangement=row['cote_rangement']
+    if cote_rangement in bibliotheque:
+        if row['titre'] != bibliotheque[cote_rangement]["Titre"]:
+            bibliotheque[cote_rangement]={'Titre':row['titre'],'Auteur':row['auteur'],'Date de Publication':row['date_publication']}
+            print(f"Le livre {bibliotheque[cote_rangement]} ---- {bibliotheque[cote_rangement]['Titre']} par {bibliotheque[cote_rangement]['Auteur']} ---- a été ajouté avec succès")
+        else:
+            print(f"Le livre {bibliotheque[cote_rangement]} ---- {bibliotheque[cote_rangement]['Titre']} par {bibliotheque[cote_rangement]['Auteur']} ---- est déjà présent dans la bibliothèque")
 csvfile.close()
-
-bibliotheque.update(bibliotheque2)
-print(bibliotheque)
-
-
-
-
-
-
 
 
 
