ba50972c28e3cf2464098a039fdd2f5019999d95
Mehdi-Chahir <babassako@hotmail.com>
2024-09-25 17:20:55 -0400

    Update TP2.py

--- TP2.py
+++ TP2.py
@@ -9,25 +9,33 @@ Noms et matricules : Nom1 (Matricule1), Nom2 (Matricule2)
 ########################################################################################################## 
 # PARTIE 1 : Création du système de gestion et ajout de la collection actuelle
 ########################################################################################################## 
-bibliotheque = {}
-
 import csv
+bibliotheque={}
+
 csvfile = open('collection_bibliotheque.csv', newline='')
 c = csv.DictReader(csvfile)
 for row in c:
-    cote_rangement = row["cote_rangement"]
-    bibliotheque[cote_rangement]= row[titre], row[auteur]
+    cote_rangement=row['cote_rangement']
+    bibliotheque[cote_rangement]={'Titre':row['titre'],'Auteur':row['auteur'],'Date de Publication':row['date_publication']}
 csvfile.close()
-
-
-
-print(f' \n Bibliotheque initiale : {bibliotheque} \n')
 
 ########################################################################################################## 
 # PARTIE 2 : Ajout d'une nouvelle collection à la bibliothèque
 ########################################################################################################## 
 
-# TODO : Écrire votre code ici
+csvfile = open('nouvelle_collection.csv', newline='')
+x = csv.DictReader(csvfile)
+for row in x:
+    cote_rangement=row['cote_rangement']
+    if cote_rangement in bibliotheque:
+        if row['titre'] != bibliotheque[cote_rangement]["Titre"]:
+            bibliotheque[cote_rangement]={'Titre':row['titre'],'Auteur':row['auteur'],'Date de Publication':row['date_publication']}
+            print(f"Le livre {bibliotheque[cote_rangement]} ---- {bibliotheque[cote_rangement]['Titre']} par {bibliotheque[cote_rangement]['Auteur']} ---- a été ajouté avec succès")
+        else:
+            print(f"Le livre {bibliotheque[cote_rangement]} ---- {bibliotheque[cote_rangement]['Titre']} par {bibliotheque[cote_rangement]['Auteur']} ---- est déjà présent dans la bibliothèque")
+csvfile.close()
+
+
 
 
 
